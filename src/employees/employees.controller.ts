31b0dfc71850930e09dc984d96700801bb0c194a
anouar garbaa <g.anouar@yahoo.com>
2025-07-24 16:05:58 +0200

    add Logger for GET Route

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,16 +7,18 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma, Role } from '@prisma/client';
 import { SkipThrottle, Throttle } from '@nestjs/throttler';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
 
 @SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
-
+  private readonly logger = new MyLoggerService(EmployeesController.name);
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
@@ -24,7 +26,8 @@ export class EmployeesController {
 
   @SkipThrottle({ default: false })
   @Get()
-  findAll(@Query('role') role?: Role) {
+  findAll(@Ip() ip: string, @Query('role') role?: Role) {
+    this.logger.log(`Request for ALL Employees\t${ip}`);
     return this.employeesService.findAll(role);
   }
 
